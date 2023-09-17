9f86268fbf543e69294092bc971cc88e21c5e37e
Shazoji90 <squash7@live.com>
2023-09-17 19:20:39 +0700

    inisiasi awal

---
+++ resources/js/components/tweet/Post.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <form action="#" @submit.prevent="post">
+            <div class="form-group mb-3">
+                <textarea
+                    cols="30"
+                    rows="2"
+                    class="form-control"
+                    v-model="body"
+                ></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Post</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            body: null,
+        };
+    },
+    props: {
+        tweets: Array,
+    },
+    methods: {
+        post() {
+            axios
+                .post("tweets", {
+                    body: this.body,
+                })
+                .then((res) => {
+                    this.tweets.unshift(res.data);
+                    this.body = null;
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        },
+    },
+};
+</script>
---
+++ resources/js/components/tweet/Timeline.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="card">
+        <div class="card-header">Timeline</div>
+        <div class="card-body">
+            <post :tweets="tweets"></post>
+            <hr />
+            <div
+                class="d-flex mb-3"
+                v-for="(tweet, index) in tweets"
+                :key="index"
+            >
+                <div class="flex-shrink-0">
+                    <img src="https://placehold.it/64x64" alt="..." />
+                </div>
+                <div class="flex-grow-1 ms-3">
+                    <h5>{{ tweet.user.name }}</h5>
+                    {{ tweet.body }}
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Post from "./Post.vue";
+export default {
+    data() {
+        return {
+            tweets: [],
+        };
+    },
+    components: {
+        Post,
+    },
+    mounted() {
+        axios
+            .get("tweets")
+            .then((res) => {
+                this.tweets = res.data;
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    },
+};
+</script>
